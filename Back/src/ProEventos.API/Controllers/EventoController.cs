acd13353982c794bb9d3709b5cbef82fea4d63a5
Lucas Lima <acdacd1212@gmail.com>
2021-10-28 11:45:00 -0300

    v.0.0.1 - ProEventos

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController(ILogger<EventoController> logger)
+        {
+
+        }
+
+        [HttpGet]
+        public List<Evento> Get()
+        {
+            return new List<Evento>(){
+                new Evento(){
+                EventoID = 1,
+                Tema = "Encontro Turma",
+                Local = "Rincão",
+                QtdPessoas = 20,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                Lote = "Primeiro",
+                ImagemUrl = "NULL",
+            },
+            new Evento(){
+                EventoID = 1,
+                Tema = "Encontro Turma",
+                Local = "Rincão",
+                QtdPessoas = 20,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                Lote = "Primeiro",
+                ImagemUrl = "NULL",
+            }
+        };
+    }
+
+    [HttpPost]
+    public string Post()
+    {
+        return "Método POST";
+    }
+
+    [HttpDelete("{ID}")]
+    public string DELETE(int ID)
+    {
+        return $"Método DROPATUDO com ID {ID}";
+    }
+}
+}
